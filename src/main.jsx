a638a2570c8b185cbb13b4ff961efcd16cb6e3dc
Claudia <holaclaudiia@gmail.com>
2023-06-07 20:19:09 -0500

    implementa la vista de registro

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Login from './login/login.jsx'
+import Mesa from './mesa/mesa.jsx'
+
+const router = createBrowserRouter([
+  { path: "/", element: <Login /> },
+  { path: "/mesa", element: <Mesa /> },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/mesa/mesa.jsx
@@ -0,0 +1,27 @@
+export default function Mesa() {
+    return (
+        <>
+            <div id="burguerQueen">
+                <h1>Carta y Cuenta</h1>
+                <div>
+                    <form id="search-form" role="search">
+
+                        <label>Client:</label>
+                        <input
+                            id="client"
+                            aria-label="enter name"
+                            placeholder="example Ana"
+                            type="text"
+                            name="client"
+                        />
+                    </form>
+
+                    <button type="submit">Send to kitchen</button>
+
+                </div>
+
+            </div>
+            <div id="detail"></div>
+        </>
+    );
+}

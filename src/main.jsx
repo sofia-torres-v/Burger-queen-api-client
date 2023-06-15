50e91803eb213e4140cd02ec4506d0cf7a5fa870
Claudia <holaclaudiia@gmail.com>
2023-06-15 13:33:40 -0500

    actualizando las ramas

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/login/login';
-import Menu from './pages/waiter/waiter';
-import Administrator from './pages/administrator/administrator';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />
-  },
-  {
-    path: '/waiter',
-    element: <Menu />
-  },
-  {
-    path: '/admin',
-    element: <Administrator />
-  },
-]);
-
-
-
+import { BrowserRouter} from 'react-router-dom';
+import Route from './route/route'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+    <Route />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-export default router;

e3bc2bc63428f1528cec420beb9dbc59079c651e
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-06-15 16:05:36 -0500

    ADD:changes in css

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -4,10 +4,10 @@ import Logo from '../../assets/12.png';
 const Header = () => {
   return (
     <div className="column-header">
-      {/* <h1>BURGER QUEEN</h1> */}
+      <h1>BURGER QUEEN</h1>
       <figure className="content-logo">
         <img src={Logo} alt="logo" />
-        <h1>BURGER QUEEN</h1>
+        {/* <h1>BURGER QUEEN</h1> */}
       </figure>
     </div>
   );

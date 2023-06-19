50b6a5b60c3d2065b3917afc1c230469a5af6261
Claudia <holaclaudiia@gmail.com>
2023-06-19 17:33:27 -0500

    ADD:function for bringing products

--- src/Components/cardElement/cardElement.jsx
+++ src/Components/cardElement/cardElement.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './cardElement.css'
 
+
 export default function CardElement({ title, price }) {
+
+  
+
   return (
-    <div className='card-element'>
+    <section className='card-element'>
       <p>{title}</p>
       <div className='group-price'>
         <p>$ <span>{price}</span></p>
         <button className='btn-card'>Add</button>
       </div>
-    </div>
+    </section>
   );
 }

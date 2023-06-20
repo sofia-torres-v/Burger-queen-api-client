2e60c7a7ea81b625170cc7d4aa172529aae3a37e
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-06-20 11:12:43 -0500

    UPDATE:Admin Screen Styles

--- src/pages/administrator/administrator.jsx
+++ src/pages/administrator/administrator.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
 import Logout from '../../Components/Logout/logout';
-import AdminUser from '../../Components/adminUser/AdminUser';
+import LogoBurger from '../../Components/Logo/logo';
+// import AdminUser from '../../Components/adminUser/AdminUser';
+import EditAndDelete from '../../Components/userButtons/EditAndDelete';
+import './administrator.css'
+
 
 
 export default function Administrator() {
-
     const user = localStorage.getItem('token')
-
 
     return (
         <>
-            <section className="global-container">
-                <Logout />
-                <AdminUser />
-                
-            </section>
+        <div className='global-container-Admi'>
+
+            <header className="header-admi container">
+                <div className='box-text-logout'> 
+                    <span>Administrator</span>
+                    <Logout />
+                </div>
+                    <LogoBurger />
+                    {/* <AdminUser />    */}
+            </header>
+
+            <main >
+                <section className='content-general-products container'>
+
+                    <h3 className='rolTitle'>Waiter</h3>
+                    <ul className='content-cards-products'>
+                        <EditAndDelete Name='American Coffe' />
+                        <EditAndDelete Name='American Coffe' />
+                        <EditAndDelete Name='American Coffe' />
+                    </ul>
+                        
+                    <h3 className='rolTitle'>Chef</h3>
+                    <ul className='content-cards-products'>
+                        <EditAndDelete Name='American Coffe' />
+                        <EditAndDelete Name='American Coffe' />
+                        <EditAndDelete Name='American Coffe' />
+                    </ul>
+
+                    <h3 className='rolTitle'>Administrator</h3>
+                    <ul className='content-cards-products'>
+                        <EditAndDelete Name='American Coffe' />
+                        <EditAndDelete Name='American Coffe' />
+                        <EditAndDelete Name='American Coffe' />
+                    </ul>
+
+                </section>
+
+            </main>
+        </div>
         </>
     );
 }

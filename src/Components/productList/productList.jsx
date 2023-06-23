a3560441f19ae4b5b805b9f31b901ba86292b8b7
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-06-23 13:58:06 -0500

    ADD:function to count products from the order list

--- src/Components/productList/productList.jsx
+++ src/Components/productList/productList.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import CardOrder from '../../Components/cardOrder/cardOrder'
 
 
-
-
 export default function ProductList({ products }) {
+
+
     return (
+
         <div className='content-product-list' >
             {products.map((product,index) => {
                 return <CardOrder 

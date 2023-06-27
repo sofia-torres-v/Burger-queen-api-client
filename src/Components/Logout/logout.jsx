75857a6b14986ea143749b636e9719e5e1f7f5bf
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-06-27 10:00:33 -0500

    ADD:Modal to the button to send orders to the kitchen with their respective styles

--- src/Components/Logout/logout.jsx
+++ src/Components/Logout/logout.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import out from '../../assets/out.png';
 import './logout.css'
 
-const Logout = () => {
+const Logout = ({text, icon}) => {
     const navigate = useNavigate();
 
     const logout = () => {
@@ -13,10 +13,19 @@ const Logout = () => {
     };
 
     return (
-        <div className='btn-logout-container'onClick={logout}>
-            <figure className='box-out'>
-                <img src={out} className='out' alt="logo"/>
-                {/* <p className='text-out'>Logout</p> */}
+        <div className='btn-logout-container'>
+            <figure className='box-content'>
+
+                <div className='box-icon'>
+                    <img src={icon} className='icon-waiter' alt="" />
+                    <p className='text-waiter'>{text}</p>
+                </div>
+
+                <div className='box-logout'onClick={logout}>
+                    <img src={out} className='out' alt="logo"/>
+                    <p className='text-out'>Logout</p>
+                </div>
+                 
             </figure>
         </div>
     );

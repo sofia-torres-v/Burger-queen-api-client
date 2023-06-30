34e52a639f162e8913aa35f655c03fbb23ba2f3d
Claudia <holaclaudiia@gmail.com>
2023-06-29 19:06:56 -0500

    ADD:card and functionality to display order in cheff

--- src/api_client/api.jsx
+++ src/api_client/api.jsx
@@ -45,6 +45,7 @@ const api = () => {
     };
 
 
+
     // Enviar lista de pedidos  a la Api
     const fetchSendOrder = async (selectedProducts, token) => {
         try {
@@ -67,6 +68,27 @@ const api = () => {
         }
     };
 
+    // trae lista de pedidos  a la Api
+    const fetchGetOrder = async ({ token }) => {
+        try {
+            const response = await fetch('http://localhost:8080/orders', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            if (response.ok) {
+                const listProducts = await response.json();
+                console.log('la lista se trajo correctamente')
+                return listProducts
+            } else {
+                console.log('Hubo un error al traer la orden')
+            }
+        } catch (error) {
+            console.log(error,'Error de la solicitud HTTP')
+        }
+    };
 
     //Cambiando el estado de la orden 
     const changeStatus = async (order, token) => {
@@ -96,6 +118,7 @@ const api = () => {
         fetchProducts,
         login,
         fetchSendOrder,
+        fetchGetOrder,
         changeStatus,
     }
 }

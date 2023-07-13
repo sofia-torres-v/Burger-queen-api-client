1d80a968c50246cd5d3f05766b7165ed1960c6a2
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-07-13 13:40:38 -0500

    ADD:four test in api file

--- src/api_client/api.test.jsx
+++ src/api_client/api.test.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Api from './api.jsx';
-// import { rest } from 'msw';
-// import { setupServer } from 'msw/node';
-// import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-
 
 describe('Api', () => {
     const token = 'validtoken123';
@@ -64,7 +60,7 @@ describe('Api', () => {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    'authorization': `Bearer ${token}`,
+                    'Authorization': `Bearer ${token}`,
                 },
             });
             expect(response).toEqual({
@@ -124,17 +120,14 @@ describe('Api', () => {
                 expect(result).toEqual(orderDate); // Verificar que la función devuelva los datos de la orden
         });
 
-        test('Deberia mostrar error cuando el token es invalido', async () => {
+        test('Deberia mostrar error cuando el token es invalido-fetchSendOrder', async () => {
             // Simular respuesta de error
             global.fetch = jest.fn(() =>
-              Promise.resolve({
-                ok: false,
-              })
+                Promise.reject(new Error())
             );
-          
-            // Envuelve la llamada a fetchSendOrder en una función asíncrona
+            // Envuelve la llamada en una función asíncrona
             const testFunction = async () => {
-              await Api().fetchSendOrder({ orderDate, token });
+                await Api().fetchSendOrder(orderDate, token);
             };
           
             // Verifica que la función arroje un error
@@ -142,139 +135,310 @@ describe('Api', () => {
           });
     })          
 
+ 
+ // trae lista de pedidos de la APi
+ describe('Función fetchGetOrder', () => {
+    test('Deberia mostrar la lista de pedidos', async () => {
+    // Simula una respuesta exitosa
+    const mockResponse = {
+        ok: true,
+        json: jest.fn().mockResolvedValue([
+          { name: 'Pollo', type: 'Desayuno' },
+          { name: 'Papas', type: 'Almuerzo' },
+          { name: 'Aros de cebolla', type: "Almuerzo" }
+        ])
+      };
+      global.fetch = jest.fn().mockResolvedValue(mockResponse);
+  
+    //   Llama a la función y verifica los resultados
+      const result = await Api().fetchGetOrder({ token });
+  
+      expect(global.fetch).toHaveBeenCalledWith('http://localhost:8080/orders', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+         'Authorization': `Bearer ${token}`,
+        }
+      });
+})
+
+test('Deberia mostrar error cuando el token es invalido -fetchGetOrder', async () => {
+    // Simular respuesta de error
+    global.fetch = jest.fn(() =>
+    Promise.reject(new Error())
+);
+// Envuelve la llamada en una función asíncrona
+const testFunction = async () => {
+    await Api().fetchGetOrder({token})
+};
+  
+    // Verifica que la función arroje un error
+    await expect(testFunction()).rejects.toThrow('Error de la solicitud HTTP');
+  });
+
+ })
+
+//Cambiando el estado de la orden 
+describe('Función changeStatus', () => {
+    test.skip('Deberia modificar el estado de la orden', async () => {
+        const order = { id: 1 };
+        const status = 'delivery';
+
+        // Simula una respuesta exitosa
+        const mockResponse = {
+            ok: true,
+            json: jest.fn().mockResolvedValue([{ id: 1, status: 'delivery' }])
+          };
+          global.fetch = jest.fn().mockResolvedValue(mockResponse);
+
+         // Llama a la función y verifica los resultados
+        const result = await Api().changeStatus(order, status, token);
+        
+    expect(global.fetch).toHaveBeenCalledWith('http://localhost:8080/orders/1', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+         'Authorization':  `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          status: 'delivery',
+          dateProcessed: new Date(),
+        })
+    })
+    expect(result).toEqual([{ id: 1, status: 'delivery' }]);
+})
+
+})
+
+
+
+// final-------------------------------------------------------------
+ 
+ // traer usuarios
+    describe('fetchShowUsers', () => {
+        test('debería devolver los usuarios filtrados por rol si la respuesta es exitosa', async () => {
+          // Simula una respuesta exitosa
+          const mockResponse = {
+            ok: true,
+            json: jest.fn().mockResolvedValue([
+              { name: 'Usuario1', role: 'admin' },
+              { name: 'Usuario2', role: 'waiter' },
+              { name: 'Usuario3', role: 'cheff' }
+            ])
+          };
+          global.fetch = jest.fn().mockResolvedValue(mockResponse);
+      
+        //   Llama a la función y verifica los resultados
+          const result = await Api().fetchShowUsers({ token });
+      
+          expect(global.fetch).toHaveBeenCalledWith('http://localhost:8080/users', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`,
+            }
+          });
+          expect(mockResponse.json).toHaveBeenCalled();
+          expect(result).toEqual({
+            admin: [{ name: 'Usuario1', role: 'admin' }],
+            waiter: [{ name: 'Usuario2', role: 'waiter' }],
+            cheff: [{ name: 'Usuario3', role: 'cheff' }]
+          });
+        });
+
+      });
+
+
+    //Crear productos
+    describe('Función fetchCreateProduct ', () => {
+        test.skip('Debe crear un producto', async () => {
+            const name = 'Papa Frita';
+            const price = 500;
+            const img = 'https://img.freepik.com/vector-gratis/papas-fritas-realistas-3d-caja-papel-rojo-condimento-mayochup-tazon_1441-2192.jpg?w=740&t=st=1689196713~exp=1689197313~hmac=a88d540b0944e540e0842b60530e4d888133590339af6c56be737eafe3f011a3';
+            const type = 'Desayuno';
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                })
+            )
+            const result = await Api().fetchCreateProduct({ token, name, price, img, type });
+            expect(fetch).toHaveBeenCalledWith(
+                'http://localhost:8080/products',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        "name": name,
+                        "price": price,
+                        "image": img,
+                        "dateEntry": new Date(),
+                        "type": type
+                    })
+                })
+        })
+    })
+    //Crear usuarios
+    describe('Función fetchCreateStaff ', () => {
+        test('Debe crear un usuario', async () => {
+            const email = 'waiter@bbq.com';
+            const password = '123456';
+            const role = 'Waiter'
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                })
+            )
+            const result = await Api().fetchCreateStaff({ token, email, password, role });
+            expect(fetch).toHaveBeenCalledWith(
+                'http://localhost:8080/users',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        "email": email,
+                        "password": password,
+                        "role": role,
+                    })
+                })
+        })
+    })
+
+
+    //editar producto
+    describe('Función fetchEditProducts ', () => {
+        const product = {
+            id: 1,
+            name: 'Papas Fritas',
+            price: 500,
+            image: 'https://img.freepik.com/vector-gratis/papas-fritas-realistas-3d-caja-papel-rojo-condimento-mayochup-tazon_1441-2192.jpg?w=740&t=st=1689196713~exp=1689197313~hmac=a88d540b0944e540e0842b60530e4d888133590339af6c56be737eafe3f011a3',
+            type: 'Desayuno'
+        };
+        test('Debe Editar a un producto especifico', async () => {
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                })
+            )
+            const result = await Api().fetchEditProducts(token, product);
+            expect(fetch).toHaveBeenCalledWith(
+                `http://localhost:8080/products/${product.id}`,
+                {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        "name": product.name,
+                        "price": product.price,
+                        "image": product.img,
+                        "type": product.type
+                    })
+                }
+            );
+        });
+    })
+    //editar staff
+    describe('Función fetchEditStaff', () => {
+        const user = { id: 1, email: 'example@example.com', role: 'admin' };
+        test('Debe Editar a un personal especifico', async () => {
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                })
+            )
+            const result = await Api().fetchEditStaff(token, user);
+            expect(fetch).toHaveBeenCalledWith(
+                `http://localhost:8080/users/${user.id}`,
+                {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        "email": user.email,
+                        "role": user.role,
+                    })
+                }
+            );
+        });
+    })
+    // Eliminar productos
+    describe('Función fetchDeleteProduct', () => {
+        const productId = { id: 1 };
+        test('Debe eliminar el producto especificado', async () => {
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                })
+            )
+            const result = await Api().fetchDeleteProduct({ token, productId });
+            expect(fetch).toHaveBeenCalledWith(
+                `http://localhost:8080/products/${productId}`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                }
+            );
+        });
+        it('Debe lanzar un error si la solicitud falla', async () => {
+            // Simular respuesta de error
+            global.fetch = jest.fn(() =>
+                Promise.reject(new Error())
+            );
+            // Envuelve la llamada en una función asíncrona
+            const testFunction = async () => {
+                await Api().fetchDeleteProduct({ token, productId });
+            };
+            // Verifica que la función arroje un error
+            await expect(testFunction).rejects.toThrow('Error de la solicitud HTTP');
+        });
+    })
+
+    // Eliminar personal
+    describe('Función fetchDeleteStaff', () => {
+        const user = { id: 1 };
+        test('Debe eliminar el usuario especificado', async () => {
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                })
+            )
+            const result = await Api().fetchDeleteStaff({ token, user });
+            expect(fetch).toHaveBeenCalledWith(
+                `http://localhost:8080/users/${user.id}`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                }
+            );
+        });
+        it('Debe lanzar un error si la solicitud falla-', async () => {
+            // Simular respuesta de error
+            global.fetch = jest.fn(() =>
+                Promise.reject(new Error())
+            );
+            // Envuelve la llamada en una función asíncrona
+            const testFunction = async () => {
+                await Api().fetchDeleteStaff({ token, user });
+            };
+            // Verifica que la función arroje un error
+            await expect(testFunction).rejects.toThrow('Error de la solicitud HTTP');
+        });
+    })
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    //    // servidor de prueba para simular la respuesta de la API
-    //     const server = setupServer(
-    //         rest.post('http://localhost:8080/orders', (req, res, ctx) => {
-    //             return res(ctx.json({ success: true }));
-    //         })
-    //     );
-
-    //     beforeAll(() => server.listen());
-    //     afterEach(() => server.resetHandlers());
-    //     afterAll(() => server.close());
-
-    //     describe('fetchSendOrder', () => {
-    //         test('envía la orden correctamente y muestra un mensaje de éxito', async () => {
-    //             const order = {
-    //                 "client": 'Claudia',
-    //                 "userId": 1,
-    //                 "products": [],
-    //                 "status": "pending",
-    //                 "dataEntry": new Date(),
-    //             };
-    //             const token = '123456';
-
-    //             render(<Api />);
-
-    //             const sendButton = screen.getByText('Send to kitchen');
-    //             fireEvent.click(sendButton);
-
-    //             await waitFor(() => screen.getByText('La orden se envió correctamente'));
-    //             // Ajusta este selector según el mensaje de éxito esperado
-
-    //             expect(fetch).toHaveBeenCalledWith('http://localhost:8080/orders', {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'Authorization': `Bearer ${token}`
-    //                 },
-    //                 body: JSON.stringify(order)
-    //             });
-    // });
-    // });
-
-
-    // describe('Función fetchShowUsers', () => {
-    //     beforeEach(() => {
-            
-    //         global.fetch = jest.fn(() =>
-    //             Promise.resolve({
-    //                 ok: true,
-    //                 json: () =>
-    //                     Promise.resolve([
-    //                         { id: 1, role: 'admin', name: 'Admin User' },
-    //                         { id: 2, role: 'waiter', name: 'Waiter User' },
-    //                         { id: 3, role: 'cheff', name: 'Cheff User' },
-    //                     ]),
-    //             })
-    //         );
-    //     });
-    //     afterEach(() => {
-    //         jest.clearAllMocks();
-    //     });
-
-    //     it('debe obtener los usuarios y filtrarlos por función', async () => {
-    //         const token = 'token123';
-    //         const expectedUsers = {
-    //             admin: [{ id: 1, role: 'admin', name: 'Admin User' }],
-    //             waiter: [{ id: 2, role: 'waiter', name: 'Waiter User' }],
-    //             cheff: [{ id: 3, role: 'cheff', name: 'Cheff User' }],
-    //         };
-    //         const { result } = renderHook(() => fetchShowUsers({ token }));
-    //         //WAITFOR: se utiliza para esperar a que se cumpla una condición antes de continuar con las comprobaciones en un test unitario.
-    //         await waitFor(() => {
-    //             expect(result.current).toEqual(expectedUsers);
-    //         });
-    //         expect(global.fetch).toHaveBeenCalledWith('http://localhost:8080/users', {
-    //             method: 'GET',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 authorization: `Bearer ${token}`,
-    //             },
-    //         });
-    //     });
-
-    //     it('should throw an error when response is not "ok"', async () => {
-    //         global.fetch.mockImplementationOnce(() =>
-    //             Promise.resolve({
-    //                 ok: false,
-    //             })
-    //         );
-    //         const token = 'token789';
-    //         const { result } = renderHook(() => fetchShowUsers({ token }));
-    //         await waitFor(() => {
-    //             expect(result.error).toEqual(Error('ERROR: token inválido'));
-    //         });
-    //         expect(global.fetch).toHaveBeenCalledWith('http://localhost:8080/users', {
-    //             method: 'GET',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 authorization: `Bearer ${token}`,
-    //             },
-    //         });
-    //     });
-
-    //     it('should throw an error when an error occurs during the request', async () => {
-    //         const expectedError = new Error('Network error');
-    //         global.fetch.mockImplementationOnce(() => Promise.reject(expectedError));
-    //         const token = 'token456';
-    //         const { result } = renderHook(() => fetchShowUsers(token));
-    //         await waitFor(() => {
-    //             expect(result.error).toEqual(expectedError);
-    //         });
-    //         expect(global.fetch).toHaveBeenCalledWith('http://localhost:8080/users', {
-    //             method: 'GET',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 authorization: `Bearer ${token}`,
-    //             },
-    //         });
-    //     });
-    // });
 });
